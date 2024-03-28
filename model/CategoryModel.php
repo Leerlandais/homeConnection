6ce0624d8c440cac312e4f2b1ca8ca90a157b2d6
leerlandais <leerlandais@gmail.com>
2024-03-28 13:28:20 +0100

    time for JS

--- model/CategoryModel.php
+++ model/CategoryModel.php
@@ -29,7 +29,7 @@ if ($cleanedSlug == "nouser") {
     ";
  
 }else {
-    $sql = "SELECT SUBSTRING(n.content, 1, 100) AS con, n.slug, n.title, n.date_published, c.title, u.thename
+    $sql = "SELECT SUBSTRING(n.content, 1, 100) AS con, n.slug, n.title AS ntit, n.date_published, c.title, u.thename
             FROM news n
             JOIN news_has_category h ON h.news_idnews = n.idnews
             LEFT JOIN category c ON c.idcategory = h.category_idcategory
